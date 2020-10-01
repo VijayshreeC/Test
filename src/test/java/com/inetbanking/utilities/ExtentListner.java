e44a311c3d53b7a5600ad2e02cd8c1582f7699c2
VijayshreeC <vijayshree.chaturvedi@citiustech.com>
2020-10-01 21:50:33 +0530

    Added Reports code

---
+++ src/test/java/com/inetbanking/utilities/ExtentListner.java
@@ -0,0 +1,167 @@
+package com.inetbanking.utilities;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.inetbanking.testCases.BaseClass;
+
+
+public class ExtentListner extends BaseClass implements ITestListener {
+	ExtentReports extent = ExtentReport.ReportGenerator();
+	ExtentTest test;
+	private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<ExtentTest>();
+
+	@Override
+	public void onTestStart(ITestResult result) {
+
+		test = extent.createTest(result.getMethod().getMethodName());
+		extentTest.set(test);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		extentTest.get().log(Status.PASS, "Test Passed");
+		Object currentClass = result.getInstance();
+		WebDriver webDriver = ((BaseClass) currentClass).driver;
+		if (webDriver != null) {
+			File scrFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);// cast driver to screenshot
+																							// mode
+			String destPath = "reports/screenshots/" + System.currentTimeMillis() + result.getMethod().getMethodName()
+					+ ".png";
+			String imagePath = "screenshots/" + System.currentTimeMillis() + result.getMethod().getMethodName()
+					+ ".png";
+			File file = new File(destPath);
+			try {
+				FileUtils.copyFile(scrFile, file);
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+			try {
+				extentTest.get().addScreenCaptureFromPath(imagePath, result.getMethod().getMethodName());
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		Object currentClass = result.getInstance();
+		WebDriver webDriver = ((BaseClass) currentClass).driver;
+		extentTest.get().fail(result.getThrowable());
+		/*
+		 * Object testObject = result.getInstance();
+		 * System.out.println("Object Instance " + testObject.toString()); Class cl =
+		 * result.getTestClass().getRealClass(); System.out.println("Class name" + cl);
+		 * try { driver = (WebDriver) cl.getDeclaredField("driver").get(testObject);
+		 * 
+		 * } catch (Exception e1) { e1.printStackTrace(); }
+		 */
+		if (webDriver != null) {
+			File scrFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);// cast driver to screenshot
+																							// mode
+			String destPath = "reports/screenshots/" + System.currentTimeMillis() + result.getMethod().getMethodName()
+					+"Failed" + ".png";
+			String imagePath = "screenshots/" + System.currentTimeMillis() + result.getMethod().getMethodName()
+					+ "Failed " + ".png";
+			File file = new File(destPath);
+			try {
+				FileUtils.copyFile(scrFile, file);
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+			try {
+				extentTest.get().addScreenCaptureFromPath(imagePath, result.getMethod().getMethodName());
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+		
+		
+	@Override
+	public void onTestSkipped(ITestResult result) {
+
+		extentTest.get().log(Status.SKIP, "Skipped");
+		Object currentClass = result.getInstance();
+		WebDriver webDriver = ((BaseClass) currentClass).driver;
+		if (webDriver != null) {
+			File scrFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);// cast driver to screenshot
+																							// mode
+			String destPath = "reports/screenshots/" + System.currentTimeMillis() + result.getMethod().getMethodName()
+					+ ".png";
+			String imagePath = "screenshots/" + System.currentTimeMillis() + result.getMethod().getMethodName()
+					+ ".png";
+			File file = new File(destPath);
+			try {
+				FileUtils.copyFile(scrFile, file);
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+			try {
+				extentTest.get().addScreenCaptureFromPath(imagePath, result.getMethod().getMethodName());
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		extentTest.get().log(Status.PASS, "Successful");
+		Object currentClass = result.getInstance();
+		WebDriver webDriver = ((BaseClass) currentClass).driver;
+		if (webDriver != null) {
+			File scrFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);// cast driver to screenshot
+																							// mode
+			String destPath = "reports/screenshots/" + System.currentTimeMillis() + result.getMethod().getMethodName()
+					+ ".png";
+			String imagePath = "screenshots/" + System.currentTimeMillis() + result.getMethod().getMethodName()
+					+ ".png";
+			File file = new File(destPath);
+			try {
+				FileUtils.copyFile(scrFile, file);
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+			try {
+				extentTest.get().addScreenCaptureFromPath(imagePath, result.getMethod().getMethodName());
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		extent.flush();
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+	}
+}
---
+++ src/test/java/com/inetbanking/utilities/ExtentReport.java
@@ -0,0 +1,28 @@
+package com.inetbanking.utilities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+public class ExtentReport {
+	static ExtentReports extent;
+
+	public static ExtentReports ReportGenerator() {
+		extent = new ExtentReports();
+
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());// time stamp
+		//String repName = "Test-Report-" + timeStamp + ".html";
+
+		//String path = "/var/jenkins_home/workspace/TestProject/reports/" + "index.html";
+		String path=System.getProperty("user.dir") + "/reports/ " + "index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Guru99Demo Automation Test Report");
+		reporter.config().setDocumentTitle("TestResults");
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "XYZ");
+		return extent;
+
+	}
+}
